3ca4c36d42cfaf2fc37a864e6c5e4d9e07cc4ca0
Olena Sakhno <65957461+OlenaSakhno@users.noreply.github.com>
2023-01-03 10:01:29 +0000

    1.2 course information, step2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,4 +1,13 @@
-const App = () => {
+const Part = (props) => {
+  return (
+    <>
+      <p>
+        {props.partNo} {props.exercises}
+      </p>
+    </>
+  );
+};
+const Content = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
   const exercises1 = 10;
@@ -6,20 +15,21 @@ const App = () => {
   const exercises2 = 7;
   const part3 = "State of a component";
   const exercises3 = 14;
-
   return (
     <div>
       <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Part partNo={part1} exercises={exercises1} />
+      <Part partNo={part2} exercises={exercises2} />
+      <Part partNo={part3} exercises={exercises3} />
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <Content />
     </div>
   );
 };
